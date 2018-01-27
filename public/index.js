8c252943299398346156027b013217f7e09d797d
Spyk1e <spykie.me@gmail.com>
2018-01-27 18:06:01 +0100

    added some css things

--- public/index.js
+++ public/index.js
@@ -2,36 +2,36 @@
 'use strict';
 
 (() => {
-  const render = (actors) => {
-    const fragment = document.createDocumentFragment();
-    const div = document.createElement('div');
-    const template = actors.map(actor => {
-      return `
+    const render = (actors) => {
+        const fragment = document.createDocumentFragment();
+        const div = document.createElement('div');
+        const template = actors.map(actor => {
+            return `
         <div class="actor col">
-          <span><h1>${actor.who}</h1></span>
-          <span><h2>${actor.type}</h2></span>
-          <span><h3>${actor.amount} \u20AC</h3></span>
+          <p><h1>${actor.who}</h1></p>
+          <p><h2>${actor.type}</h2></p>
+          <p><h3>${actor.amount} \u20AC</h3></p>
         </div>
       `;
-    }).join('');
+        }).join('');
 
-    div.innerHTML = template;
-    fragment.appendChild(div);
-    document.querySelector('#actors').innerHTML = '';
-    document.querySelector('#actors').appendChild(fragment);
-  };
+        div.innerHTML = template;
+        fragment.appendChild(div);
+        document.querySelector('#actors').innerHTML = '';
+        document.querySelector('#actors').appendChild(fragment);
+    };
 
-  const button = document.querySelector('#compute');
+    const button = document.querySelector('#compute');
 
-  button.addEventListener('click', function onClick () {
-    const trucker = CONVARGO.getTrucker();
-    const distance = document.querySelector('.distance').value;
-    const volume = document.querySelector('.volume').value;
-    const option = document.querySelector('.option').checked;
-    const actors = CONVARGO.payActors(trucker, distance, volume, option);
+    button.addEventListener('click', function onClick() {
+        const trucker = CONVARGO.getTrucker();
+        const distance = document.querySelector('.distance').value;
+        const volume = document.querySelector('.volume').value;
+        const option = document.querySelector('.option').checked;
+        const actors = CONVARGO.payActors(trucker, distance, volume, option);
 
-    render(actors);
+        render(actors);
 
-    return;
-  });
+        return;
+    });
 })();
